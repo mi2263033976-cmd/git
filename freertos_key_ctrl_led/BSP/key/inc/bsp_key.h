bb4abea3351f2c7831838fa87233bb4f24a74eff
mi2263033976-cmd <mi2263033976@gmail.com>
2026-09-06 22:50:52 +0800

    feat: add FreeRTOS key control LED project (STM32F411)

---
+++ freertos_key_ctrl_led/BSP/key/inc/bsp_key.h
@@ -0,0 +1,32 @@
+/********************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ *
+ * ALL Rights Reserved.
+ *
+ * @file bsp_key.h
+ *
+ * @par dependencies
+ * - bsp_key.h	
+ * - stdio.h
+ * - stdint.h
+ *
+ * @author BUBUGou
+ *
+ * @brief Provide the HAL key of AHT21 and corresponding opetions.
+ *
+ * Processing flow:
+ *
+ * call directly.
+ *
+ * @version V1.0 2026.9.6
+ *
+ * @note 1 tab == 4 spaces!
+ *
+ ********************************************************************************/
+
+#ifndef __BSP_KEY_H__
+#define __BSP_KEY_H__
+
+
+
+#endif /* __BSP_KEY_H__ */
---
+++ freertos_key_ctrl_led/BSP/key/src/bsp_key.c
@@ -0,0 +1,27 @@
+/********************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ *
+ * ALL Rights Reserved.
+ *
+ * @file bsp_key.c
+ *
+ * @par dependencies
+ * - bsp_key.h	
+ * - stdio.h
+ * - stdint.h
+ *
+ * @author BUBUGou
+ *
+ * @brief Provide the HAL key of AHT21 and corresponding opetions.
+ *
+ * Processing flow:
+ *
+ * call directly.
+ *
+ * @version V1.0 2026.9.6
+ *
+ * @note 1 tab == 4 spaces!
+ *
+ ********************************************************************************/
+
+#include "bsp_key.h"
